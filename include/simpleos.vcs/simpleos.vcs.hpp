796e170a4953dbda7ab3d4319c4c60e5082e916e
Felipe Faria <felipeasf@gmail.com>
2019-03-21 16:42:02 -0300

    add binary info

--- include/simpleos.vcs/simpleos.vcs.hpp
+++ include/simpleos.vcs/simpleos.vcs.hpp
@@ -7,12 +7,22 @@ using namespace eosio;
 using namespace std;
 
 namespace simpleos {
+
+    struct [[eosio::table("binary.info"), eosio::contract("simpleos.vcs")]] binary {
+
+        checksum256 checksum;
+        string binary_name;
+        string binary_link;
+
+        EOSLIB_SERIALIZE(binary, (checksum)(binary_name)(binary_link))
+    };
+
     struct [[eosio::table("version.info"), eosio::contract("simpleos.vcs")]] version_info {
         string link;
-        checksum256 checksum;
         string version_number;
+        vector<binary> binaries;
 
-        EOSLIB_SERIALIZE(version_info, (link)(checksum)(version_number))
+        EOSLIB_SERIALIZE(version_info, (link)(version_number)(binaries))
     };
     typedef singleton<name("version.info"), version_info> version_info_singl;
 
@@ -21,6 +31,6 @@ namespace simpleos {
             version_control_system(name recvr, name code, datastream<const char*> ds);
 
             [[eosio::action("newversion")]]
-            void newversion(string link, checksum256 checksum, string version_number);
+            void newversion(string link, string version_number, vector<binary> binaries);
     };
 }//simpleos namespace
--- src/simpleos.vcs.cpp
+++ src/simpleos.vcs.cpp
@@ -3,12 +3,12 @@
 simpleos::version_control_system::version_control_system(name recvr, name code,
     datastream<const char*> ds) : contract(recvr, code, ds) {}
 
-void simpleos::version_control_system::newversion(string link, checksum256 checksum,
-    string version_number) {
+void simpleos::version_control_system::newversion(string link, string version_number, vector<binary> binaries) {
     require_auth(_self);
 
     version_info_singl data(_self, _self.value);
-    data.set(version_info{link, checksum, version_number}, _self);
+
+    data.set(version_info{link, version_number, binaries}, _self);
 }
 
 EOSIO_DISPATCH(simpleos::version_control_system, (newversion))
